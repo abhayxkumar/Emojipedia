7c1175ebf8560efa5f6796d3e8af7559d5ab8073
Abhay Kumar <65169803+abhay1144@users.noreply.github.com>
2020-06-13 22:47:04 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Emojipedia
-Created with CodeSandbox
---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 5,
+    emoji: "🤨",
+    name: "Face With a Raised Eyebrow",
+    meaning:
+      "With the questioning look on the face, resembles the actor “The Rock“ (Dwayne Johnson). Can be used to express skepticism, disbelief or disapproval."
+  },
+  {
+    id: 6,
+    emoji: "🤬",
+    name: "Face With Symbols Over The Mouth",
+    meaning:
+      "The symbols over the mouth represent cursing. The serious-looking smiley is really upset and keeps using four-letter words. Represents a sudden outburst of fury or frustration."
+  },
+  {
+    id: 7,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  },
+  {
+    id: 8,
+    emoji: "😷",
+    name: "Face With Medical Mask",
+    meaning:
+      "The mask is for protection. Either to protect yourself or others from infection. Someone is in the hospital, has to go to the doctor or has caught a disease. Wearing the masks is widely spread in Asia."
+  },
+  {
+    id: 9,
+    emoji: "😈",
+    name: "Smiling Face With Horns",
+    meaning:
+      "Mischievously grinning goblin smiley with small devil horns. Wants to cause trouble, is a real teaser or planning something ugly just now."
+  }
+];
+
+export default emojipedia;
